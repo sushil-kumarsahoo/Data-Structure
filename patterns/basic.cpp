06eda1ebbf16c25632725f55a43f2f73005187b1
sushil-kumarsahoo <suhilkmrsho@gmail.com>
2024-09-15 08:33:46 +0530

    first commit

---
+++ patterns/basic.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printname
+(string name){
+     cout << "hey " << name;
+}
+int main(){
+    string name;
+    cin >> name;
+printname
+(name);
+    return 0;
+}
