a084def3d30a01cbab850946d45f0ab72b88a56b
sushil-kumarsahoo <suhilkmrsho@gmail.com>
2024-09-25 16:00:16 +0530

    sorting

---
+++ Basic Maths/bitwiseoperator.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+     
+     int a = 4;
+     int b = 6;
+
+     cout << (a&b) << endl;
+     cout << (a|b) << endl;
+     cout << (~a)  << endl;
+     cout << (a^b) << endl;
+
+     int i = 7;
+     cout << (++i) << endl;
+     cout << (i++) << endl;
+     cout << (i--) << endl;
+     cout << (--i) << endl;
+     return 0;
+}
