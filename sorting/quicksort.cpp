3a08a892f47417cb0c5cc77729a8f0623bc904a9
sushil-kumarsahoo <suhilkmrsho@gmail.com>
2024-09-30 14:57:07 +0530

    quick sort

---
+++ sorting/quicksort.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int partition(vector<int> &arr, int low, int high)
+{
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while(i<j){
+    while (arr[i] <= pivot && i <= high - 1)
+    {
+        i++;
+    }
+    while (arr[j] > pivot && j >= low + 1)
+    {
+        j--;
+    }
+    if (i < j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low],arr[j]);
+    return j;
+}
+void qs(vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
+}
+
+vector<int> quicksort(vector<int> &arr, int low, int high)
+{
+    qs(arr, 0, arr.size() - 1);
+    return arr;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int low = 0, high = n - 1;
+    quicksort(arr, low, high);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
